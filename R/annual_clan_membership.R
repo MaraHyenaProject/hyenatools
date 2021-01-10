a0fbe4b12d9d8c8ae07020f878fbe13361ee035d
Eli Strauss <straussed@gmail.com>
2021-01-10 12:41:47 +0100

    update annual_clan_membership error checking and documentation

--- R/annual_clan_membership.R
+++ R/annual_clan_membership.R
@@ -1,9 +1,15 @@
 #' Identify living hyenas in specified clan on an annual basis
 #'
-#' Identifies all hyenas alive in a specified year for the given clan.
+#' Identifies all hyenas alive in a specified year for the given clan. This count
+#' includes any individuals who were considered immigrants or residents and who
+#' were alive and in the clan for at least one day during the year.
 #'
 #' @param clan One (or more) clan names, supplied as a character vector.
 #' @param years One (or more) year, supplied as a numeric vector
+#'
+#' @return Returns tblClanMembership filtered to all individuals who were a member
+#' of the supplied clan at some point in the supplied year. Columns include the
+#' start and end date of membership in the clan.
 #'
 #' @examples
 #' ## returns all hyenas from serena clans 2010-2012
@@ -13,18 +19,27 @@
 #'
 
 annual_clan_membership <- function(clans, years){
+  ##############################################################################
+  ### Error checking ###
   if(any(is.na(years)))
     warning('years contains NAs')
   if(any(!as.numeric(years))){
     error('Please provide years as numbers')
   }
-  if(!exists('tblClanMembership'))
+
+  if(!'hyenadata' %in% names(sessionInfo()$otherPkgs))
+    warning('hyenadata package not loaded. This function may not work as expected.')
+
+  if(!exists('tblClanMembership')){
     data(tblClanMembership)
-  
+    warning('tblClanMembership not in environment. Loading tblClanMembership from hyenadata package')
+  }
+
+  ##############################################################################
   tcm <- tblClanMembership
   tcm$start_year <- as.numeric(format(tcm$start_date, '%Y'))
   tcm$end_year <- as.numeric(format(tcm$end_date, '%Y'))
-  
+
   df <- data.frame()
   for(y in years){
     n.df <- cbind(data.frame(year = y),
